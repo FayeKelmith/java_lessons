45e27b18d8730e262b13f19f03e9312fe1b4f439
fayekelmith <faye.12222883@lpu.in>
2024-01-14 10:48:51 +0530

    fundamentals and cntrls

--- class_lessons/01_hello/hello.java
+++ class_lessons/01_hello/hello.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 class Hello{
     public static void main(String args[]){
-        System.out.println("Hello World");
+
+        Scanner input = new Scanner(System.in);
+        //just prints the string in quotes to terminal
+        System.out.print("Java is not that bad!");
+        //does not move to next line before printing. Just prints and then moves to next line
+        System.out.println("\nWe could get something going!");
+        //Just like C. Moves to the next line, prints, then stays on that line
+        System.out.printf("Let's see where this goes!\n");
+
+        //Float vs Double
+        float f = 1.2345678901234567890f;
+        double d = 1.2345678901234567890;
+        double anotherDouble = 1.223e-5;
+
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Another Double: " + anotherDouble);
+        
+        //we need to add the f to the end of the float to make it a float
+        //otherwise it will be a double
+        //we can use e to make exponents
+        System.out.println("Your name please: ");
+        String name = input.nextLine();
+        System.out.println("You entered: " + name);
+    
+        System.out.println("\nCan I get a number: ");
+        int number = input.nextInt();
+        System.out.println("You entered: " + number);
     }
 }
---
+++ class_lessons/02_controll/controll.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Controll{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        // String lang = input.next();
+        String lang = "Java";
+
+        for(int i=0; i<5;++i)
+            System.out.println("Java is fun "+ i);
+
+        if(lang.equals("Java")){
+            System.out.println("Hello Java");
+        }
+        else if(lang.equals("C")){
+            System.out.println("Hello C");
+        }
+        else{
+            System.out.println("Hello World");
+        }
+
+        switch (lang) {
+            case "Java":
+                System.out.println("I was watching too: Java");
+                break;
+            case "C":
+                System.out.println("I was watching too: C");
+                break;
+        
+            default:
+                System.out.println("We ain't got that here!");
+                break;
+        }
+
+        // int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        String[] numbers = {"one","two","three","four","five","six","seven","eight","nine","ten"};
+        for(String num: numbers){
+            System.out.println("Number " + num);
+        }
+
+
+
+        input.close();
+    }
+}
